e921633fe8fbbb833b5fafb33eca3d4e027cd74d
Daniel <kingsantha16af@gmail.com>
2019-09-23 12:23:27 +0700

    Menambahkan fungsi link handler pada views.py dalam djanggo_app

--- django_project/django_app/views.py
+++ django_project/django_app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def index(request):
+    return render(request, 'django_app/index.html')
+
+def speech_to_text(request):
+    return render(request, 'django_app/speech-to-text.html')
+
+def speech_to_text_edit(request):
+    return render(request, 'django_app/speech-to-text-edit.html')
+
+def audio_upload(request):
+    return render(request, 'django_app/audio-upload.html')
+
+def text_grading(request):
+    return render(request, 'django_app/text-grading.html')
+
+def text_grading_score(request):
+    return render(request, 'django_app/text-grading-score.html')
