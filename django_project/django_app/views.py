5ac34093645701fc9982b472a8da6933435315ab
jofardo <jofardo52@gmail.com>
2019-09-24 19:44:00 +0700

    update views.py menambah controller view dari database ke html

--- django_project/django_app/views.py
+++ django_project/django_app/views.py
@@ -1,20 +1,28 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.template.response import TemplateResponse
+from .models import Audio, Text
 
 # Create your views here.
 def index(request):
     return render(request, 'django_app/index.html')
 
 def speech_to_text(request):
-    return render(request, 'django_app/speech-to-text.html')
+    data2 = Text.objects.all()
+    return render(request, 'django_app/speech-to-text.html', {"data2" : data2})
 
 def speech_to_text_edit(request):
     return render(request, 'django_app/speech-to-text-edit.html')
 
 def audio_upload(request):
-    return render(request, 'django_app/audio-upload.html')
+    data3 = Audio.objects.all()
+    return render(request, 'django_app/audio-upload.html', {"data3" : data3})
 
 def text_grading(request):
-    return render(request, 'django_app/text-grading.html')
+    data1 = Grade.objects.all()
+    return render(request, 'django_app/text-grading.html', {"data1" : data1})
 
 def text_grading_score(request):
     return render(request, 'django_app/text-grading-score.html')
+
+def detail_data(request):
+    return render(request, 'django_app/detail-data.html')
