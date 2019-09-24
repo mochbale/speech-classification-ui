0965564b710bfa9e5f4f1c01e79d7947404a5217
Daniel <kingsantha16af@gmail.com>
2019-09-24 13:39:48 +0700

    register db

--- django_project/django_app/admin.py
+++ django_project/django_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django_app.models import Audio, Text, Grade
 
 # Register your models here.
+
+admin.site.register(Audio)
+admin.site.register(Text)
+admin.site.register(Grade)
